ed92b7cb1f54eded2e63614dcde08afb7663bde6
Judah Doupe <judahdoupe@gmail.com>
2017-06-30 22:19:31 -0400

    added independent camera

---
+++ Assets/Source/Characters/Player/PlayerCameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour {
+
+    public PlayerController player;
+    public float distance = 5;
+	
+	// Update is called once per frame
+	void Update () {
+        UpdatePosition();
+
+    }
+
+    void UpdatePosition()
+    {
+        var x = player.transform.position.x;
+        var y = player.transform.position.y + distance;
+        var z = player.transform.position.z - distance;
+        transform.position = new Vector3(x, y, z);
+    }
+}
