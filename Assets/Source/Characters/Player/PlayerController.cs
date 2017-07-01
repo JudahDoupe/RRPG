5115e2938afe6953517e3fc3a7d27863ff87c3dd
Judah Doupe <judahdoupe@gmail.com>
2017-06-30 20:03:54 -0400

    updated world scene

--- Assets/Source/Characters/Player/PlayerController.cs
+++ Assets/Source/Characters/Player/PlayerController.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float SpeedMultiplier = 1;
+    public float Movement { get { return Input.GetAxis("Vertical") * SpeedMultiplier; } }
+
+
+    public float RotationMultiplier = 5;
+    public float Rotation{ get{ return Input.GetAxis("Horizontal") * RotationMultiplier; } }
+
+
+
     public void Update()
     {
         Move();
+        Rotate();
     }
-
 	private void Move () {
-        gameObject.transform.Translate(GetMovementVector());
+        gameObject.transform.Translate(0,0, Movement);
 	}
-
-    private Vector3 GetMovementVector()
+    private void Rotate()
     {
-        return new Vector3 {x = Input.GetAxis("Horizontal"), z = Input.GetAxis("Vertical") };
+        gameObject.transform.Rotate(0,Rotation,0);
     }
+
 }
